8abae9360f568bfd0b77dc88b6a7e579ff3a4f41
Atul Varma <varmaa@gmail.com>
2014-01-21 13:52:30 -0500

    added a kind of janky search box.

--- static/js/directory.js
+++ static/js/directory.js
@@ -4,3 +4,16 @@ $("body").on("click", ".js-edit-org-info", function() {
 
   button.fadeOut(function() { formArea.fadeIn(); });
 });
+
+$('input.typeahead-orgs').typeahead({
+  name: 'orgs',
+  local: $('h2 > a').map(function() {
+    return {
+      value: $.trim($(this).text()),
+      href: $(this).attr('href')
+    }
+  })
+}).on('typeahead:selected', function(e, datum) {
+  $(this).typeahead('setQuery', '');
+  window.location.hash = datum.href;
+});
